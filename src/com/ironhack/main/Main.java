765ac71e934bdf1956bbcd99d874eed697e94fb6
Galo García Pugliese <galo130@gmail.com>
2021-01-21 19:20:53 +0100

    Export party to csv file working

--- src/com/ironhack/main/Main.java
+++ src/com/ironhack/main/Main.java
@@ -111,8 +111,8 @@ public class Main {
                 if (firstParty.size() != 0 || secondParty.size() != 0) {
                     System.out.println("Wanna export the winner team to a file? Type 1[Yes] / 2[No]");
                     option = Input.getInputNumber(1, 2);
-//                    if (option == 1)
-//                        savePartyToFile();
+                    if (option == 1)
+                        savePartyToFile();
                 }
                 System.out.println("\nWanna play again? Type 1[Yes] / 2[No]");
                 option = Input.getInputNumber(1, 2);
@@ -304,7 +304,7 @@ public class Main {
                 }
             } else {
                 // Randomly generated characters.
-                party.add(RandomGenerator.generateRandomCharacter(party, i+1));
+                party.add(RandomGenerator.generateRandomCharacter(party, i + 1));
             }
         }
         return party;
@@ -313,7 +313,7 @@ public class Main {
     public static List<Character> generateGroup(int quantity, int index) {
         List<Character> characters = new ArrayList<>();
         for (int i = 0; i < quantity; i++) {
-            characters.add(RandomGenerator.generateRandomCharacter(characters, i+index));
+            characters.add(RandomGenerator.generateRandomCharacter(characters, i + index));
         }
         return characters;
     }
@@ -383,27 +383,37 @@ public class Main {
         System.out.println(ConsoleColors.WHITE_BOLD);
     }
 
-//    public static void savePartyToFile() {
-//        ArrayList<Character> winners = firstParty.size() == 0 ? originalSecondParty : originalFirstParty;
-//
-//        String fileName = Input.getFileName();
-//        String filePath = "src/com/ironhack/resources/" + fileName + ".csv";
-//        try {
-//            FileWriter fw = new FileWriter(filePath);
-//            fw.write("\"Type\", \"id\", \"Name\", \"HP\", \"Stamina/Mana\", \"Strength/Intelligence\"");
-//            for (int i=0;i<winners.size(); i++) {
-//                if(getType(winners.get(i)).equals("Warrior")){
-//
-//                    fw.write("\"" + getType(winners.get(i)) + "\"" + ", "
-//                        + winners.get(i) + ", "
-//                        + ch.getName() + ", "
-//                        + ch.getHp() + ", "
-//                        + ch.() + ", "
-//                        + ch.getId() + ", ");
-//                }
-//            }
-//        } catch (IOException e) {
-//            System.err.println("Sorry, couldn't create the file. Contact Admin.");
-//        }
-//    }
+    public static void savePartyToFile() {
+        ArrayList<Character> winners = firstParty.size() == 0 ? originalSecondParty : originalFirstParty;
+
+        String fileName = Input.getFileName();
+        String filePath = "src/com/ironhack/resources/" + fileName + ".csv";
+        try {
+            FileWriter fw = new FileWriter(filePath, false);
+            fw.write("\"Type\", \"id\", \"Name\", \"HP\", \"Stamina/Mana\", \"Strength/Intelligence\"\n");
+            for (int i = 0; i < winners.size(); i++) {
+                if (getType(winners.get(i)).equals("Warrior")) {
+                    Warrior winner = (Warrior) winners.get(i);
+                    fw.write("\"" + getType(winners.get(i)) + "\"" + ", "
+                            + winner.getId() + ", "
+                            + winner.getName() + ", "
+                            + winner.getHp() + ", "
+                            + winner.getStamina() + ", "
+                            + winner.getStrength() + "\n");
+                } else {
+                    Wizard winner = (Wizard) winners.get(i);
+                    fw.write("\"" + getType(winners.get(i)) + "\"" + ", "
+                            + winner.getId() + ", "
+                            + winner.getName() + ", "
+                            + winner.getHp() + ", "
+                            + winner.getMana() + ", "
+                            + winner.getIntelligence() + "\n");
+                }
+            }
+            fw.close();
+            System.out.println("Team saved in 'Resources' folder as '" + fileName + ".csv'");
+        } catch (IOException e) {
+            System.err.println("Sorry, couldn't create the file. Contact Admin.");
+        }
+    }
 }
